4a48d5679dffbe1f2cf85970ea73efbb82911d0f
markus <markus.s.gustafsson@gmail.com>
2022-09-08 10:40:04 +0200

    matahund

---
+++ matahunden.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#Fråga användaren om hunden är hungrig
+#skriv ut mata hunden om svar 'ja'. skriv ut ''
+
+svar = input('Är mister doggo hungrig?')
+if svar == 'ja':
+  print ("morr!!")
+elif svar=='nej':
+   print("voff!")
+else:
+   print('förstår ej')
+
+
